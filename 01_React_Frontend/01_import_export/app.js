855d5d97033b97fd229d3ebb8071ffa282accf3f
Subhasish-MoveCore <subhasishsahoo1357@gmail.com>
2026-05-10 23:29:27 +0530

    import-export using js

---
+++ 01_React_Frontend/01_import_export/app.js
@@ -0,0 +1,9 @@
+// default export
+
+// var a = 20
+// export default a
+
+const user = 'subhasish'
+export const age = 24
+export const skills = ['Frontend','Backend','Database']
+export default user
---
+++ 01_React_Frontend/01_import_export/script.js
@@ -0,0 +1,11 @@
+// default  export
+// import a from './app.js'
+// console.log(a)
+
+import {age,skills} from './app.js'
+import naam from './app.js'
+import user2 from './test.js'
+console.log(naam)
+console.log(user2)
+console.log(age)
+console.log(skills)
