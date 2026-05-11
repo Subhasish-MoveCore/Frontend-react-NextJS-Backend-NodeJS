28696af08bd76128eec4d765ed82dbaa36c349f0
Subhasish-MoveCore <subhasishsahoo1357@gmail.com>
2026-05-11 21:22:43 +0530

    React-Component

---
+++ 01_React_Frontend/03_Components/src/Components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div class="card">
+    <img src="https://picsum.photos/400/200" alt="image" />
+    <div class="card-content">
+        <h2>Creative Design</h2>
+        <p>Modern card with hover animation and gradient background.</p>
+        <a href="#" class="btn">Explore</a>
+    </div>
+</div>
+  )
+}
+
+export default Card
